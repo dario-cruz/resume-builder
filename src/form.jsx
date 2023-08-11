9482dc6d133431db3d82cac14e483e949a06f0c1
Dario Cruz <rio2012@gmail.com>
2023-08-11 08:44:48 -0400

    Add form component.

---
+++ src/form.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import NameInput from './inputs/nameInput'
+
+export default function ResumeForm( ) {
+    return(
+        <NameInput labelTitle="Name" />
+    )
+}
--- src/inputs/textInput.jsx
+++ src/inputs/textInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function textInput({ title }) {
+export default function TextInput({ title }) {
     const [input, setInput] = useState('')
 
     return (
