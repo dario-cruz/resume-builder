d0aa87f78645e5e19c040f48fad6eeb584686503
Dario Cruz <rio2012@gmail.com>
2023-08-22 13:44:21 -0400

    Add function to handle and prevent the normal form behaviour.

--- src/form.jsx
+++ src/form.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import NameInput from './inputs/nameInput'
 
 export default function ResumeForm( ) {
+    const handleSubmit = (e) => {
+        //Prevent the form from refreshing the page and loosing the users state in the application. 
+        e.preventDefault()
+    }
+
     return(
-        <NameInput labelTitle="Name" />
+        <form onSubmit={handleSubmit}>
+            <NameInput labelTitle="Name" />
+        </form>
     )
 }
