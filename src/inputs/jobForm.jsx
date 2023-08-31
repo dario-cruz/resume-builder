146b934c5a04afa1fe618032620c20dc069fb9f0
Dario Cruz <rio2012@gmail.com>
2023-08-31 09:21:25 -0400

    Fleshed out elements and layout for jobForm.

--- src/inputs/jobForm.jsx
+++ src/inputs/jobForm.jsx
@@ -6,6 +6,13 @@ export default function JobItem() {
     const [jobTitle, setJobTitle] = useState('')
     const [jobDetails, setJobDetails] = useState('')
 
+    // Define the current date and then update start and end date. 
+    let currentDate = new Date()
+    currentDate = currentDate.toISOString
+    setStartDate(currentDate)
+    setEndDate(currentDate)
+
+
     // Create function to handle the submission of form and push data to object.
     const handleSubmit = (e) => {
         // Prevent the default form behavior. 
@@ -15,10 +22,36 @@ export default function JobItem() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form className='job-form' onSubmit={handleSubmit}>
             <label>
                 Job Title:
-                <input type='text' value={jobTitle}></input>
+                <input 
+                type='text' 
+                onChange={e => setJobTitle(e.target.value)} 
+                value={jobTitle} />
+            </label>
+            <label>
+                Start Date:
+                <input 
+                type='date'
+                onChange={e => setStartDate(e.target.value)}
+                value={startDate}    
+                />
+            </label>
+            <label>
+                End Date:
+                <input 
+                type='date'
+                onChange={e => setEndDate(e.target.value)}
+                value={endDate}
+                />
+            </label>
+            <label>
+                <input
+                type='textarea'
+                onChange={e => setJobDetails(e.target.value)}
+                value={jobDetails}
+                />
             </label>
         </form>
     )
