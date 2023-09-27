394da6f347f3fb14f8eacc54d6045708a971d496
Dario Cruz <rio2012@gmail.com>
2023-09-27 10:07:58 -0400

    Add content to landing component. Add stock image for landing page use.

--- src/landing.jsx
+++ src/landing.jsx
@@ -1,10 +1,17 @@
 import './landing.css'
+import NormalText from './page_elements/normal-text'
+import HeaderText from './page_elements/header-text'
 
 export default function landing() {
 
     return (
         <section>
-            
+            <div>
+                <HeaderText content={'Resumo'} elementClass={'header-text'} />
+                <NormalText content={
+                    'The Premier Way to Build Out Your Resume and Stand Out to Employers!'
+                } />
+            </div>
         </section>
     )
 }
