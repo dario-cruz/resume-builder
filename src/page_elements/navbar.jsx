bf14af8f15840157b8ba2807c15253661893b676
Dario Cruz <rio2012@gmail.com>
2023-09-29 17:11:35 -0400

    Install npm module, iconoir

--- src/page_elements/navbar.jsx
+++ src/page_elements/navbar.jsx
@@ -3,13 +3,13 @@ export default function NavBar() {
     return (
         <nav>
             <div>
-                
+                <a href="#about-me">Resumo!</a>
             </div>
             <div>
-
+                <a href="#about-author">About The Dev!</a>
             </div>
             <div>
-
+                <a href="git-hub">GitHub</a>
             </div>
         </nav>
     )
