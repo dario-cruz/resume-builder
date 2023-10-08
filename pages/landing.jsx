86aa0418acadaf74dbf1ca36603a4270de3d1e9c
Dario Cruz <rio2012@gmail.com>
2023-10-08 14:50:20 -0400

    Removed styling from index.css, Add google font to index.html. Add styling to landing header and text.

--- pages/landing.jsx
+++ pages/landing.jsx
@@ -16,10 +16,14 @@ export default function Landing() {
     return (
         <section className='Landing-Section'>
             <div className='Landing-Section-Text'>
-                <HeaderText content={'Resumo'} elementClass={'Landing-Header'} />
-                <NormalText content={
-                    'The Premier Way to Build Out Your Resume and Stand Out to Employers!'
-                } elementClass={'Landing-Text'} />
+                <div className='Landing-Header-Container'>
+                    <HeaderText content={'Resumo'} elementClass={'Landing-Header'} />
+                </div>
+                <div className="Landing-Text-Container">
+                    <NormalText content={
+                        'The Premier Way to Build Out Your Resume and Stand Out to Employers!'
+                    } elementClass={'Landing-Text'} />
+                </div>
             </div>
             <LandingImage />
         </section>
