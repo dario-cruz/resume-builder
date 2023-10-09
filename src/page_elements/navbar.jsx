fa928a1b9f5359d6776e5e77f56d80512cfbaf2f
Dario Cruz <rio2012@gmail.com>
2023-10-09 14:48:19 -0400

    Add navbar.css. Add styling in navbar.css which applies to navbar component.

--- src/page_elements/navbar.jsx
+++ src/page_elements/navbar.jsx
@@ -1,15 +1,19 @@
+import './navbar.css'
+
 export default function NavBar() {
 
     return (
-        <nav>
-            <div>
+        <nav className="Landing-Nav">
+            <div className="Nav-Title">
                 <a href="#about-me">Resumo!</a>
             </div>
-            <div>
-                <a href="#about-author">About The Dev!</a>
-            </div>
-            <div>
-                <a href="git-hub">GitHub</a>
+            <div className="Nav-Links">
+                <div className="About-Link-Container">
+                    <a href="#about-author">About The Dev!</a>
+                </div>
+                <div className="GitHub-Link-Container">
+                    <a href="git-hub">GitHub</a>
+                </div>
             </div>
         </nav>
     )
