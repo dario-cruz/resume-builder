7d0e9e6771e50805351ae887c9bf51e41011865d
Dario Cruz <rio2012@gmail.com>
2023-10-16 14:12:15 -0400

    Add components to result-page.jsx

--- src/pages/result-page.jsx
+++ src/pages/result-page.jsx
@@ -1,5 +1,31 @@
 import resumeData from "../data/resumeData"
 
+function FullName() {
+    return (
+        <h2 className="result-name">{resumeData.firstName} {resumeData.lastName}</h2>
+    )
+}
+
+function Email() {
+    return (
+        <h2 className="result-email">{resumeData.email}</h2>
+    )
+}
+
+
+function ResumeElement({elementType, designatedClass, content}) {
+    return (
+        
+    )
+}
+
+
+
+
+
+
+
+
 export default function ResultPage() {
     
     return (
