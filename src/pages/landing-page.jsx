862318169064d3d7d3bcd6fa2a015a0310faf6a9
Dario Cruz <rio2012@gmail.com>
2023-11-03 15:55:55 -0400

    Add routing, changed css for landing page, change image for landing page.

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,32 +1,33 @@
 import './landing-page.css'
 import NormalText from '../page_elements/normal-text'
 import HeaderText from '../page_elements/header-text'
-import NavBar from '../page_elements/navbar'
 
 function LandingImage() {
     return (
         <div className='Landing-Image-Container'>
-            <img className='Landing-Image' src='../src/images/landing-illustration.svg' alt='Resumo Landing Image'/>
+            <img className='Landing-Image' src='../src/images/resume-stock-photo.jpg' alt='Resumo Landing Image'/>
         </div>
     )
 }
 
+const headingText = 'Your Resume could use a level up'
+const thePitch = 'Resume is the fastest way to create a beautiful CV that will lead to your next opportunity. Lets get started on leveling up your career, you disserve it!'
 
 export default function LandingPage() {
 
     return (
-        <section className='Landing-Section'>
-            <div className='Landing-Section-Text'>
-                <div className='Landing-Header-Container'>
-                    <HeaderText content={'Resumo'} elementClass={'Landing-Header'} />
+        <div className="landing">
+            <section className='Landing-Section'>
+                <div className='Landing-Section-Text'>
+                    <div className='Landing-Header-Container'>
+                        <HeaderText content={ headingText } elementClass={'Landing-Header'} />
+                    </div>
+                    <div className="Landing-Text-Container">
+                        <NormalText content={ thePitch } elementClass={'Landing-Text'} />
+                    </div>
                 </div>
-                <div className="Landing-Text-Container">
-                    <NormalText content={
-                    'The Premier Way to Build Out Your Resume and Stand Out to Employers!'
-                    } elementClass={'Landing-Text'} />
-                </div>
-            </div>
-            <LandingImage />
-        </section>
+                <LandingImage />
+            </section>
+        </div>
     )
 }
